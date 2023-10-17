06b26996b0539020eba27193bb812aeb1bff087f
Jay Lloyd Daquipa <31461040+JLDaquipa@users.noreply.github.com>
2023-10-17 18:14:29 +0800

    Implemented features that show summary history with the links and added a section that display the article summary

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,24 +4,47 @@ import { copy, linkIcon, loader, tick } from '../assets'
 import { useLazyGetSummaryQuery } from '../services/article'
 
 function Demo() {
+
   const [article, setArticle] = useState({
     url: '',
     summary: ''
   })
+  const [allArticles, setAllArticles] = useState([])
+  const [copied, setCopied] = useState("")
 
-  const [getSummary, {error, isFetchig}] = useLazyGetSummaryQuery();
+  const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
 
-  const handleSubmit = async (e) => {
+  useEffect(()=>{
+    const articleFromLocalStorage = JSON.parse(
+      localStorage.getItem('articles')
+    )
+
+    if(articleFromLocalStorage){
+      setAllArticles(articleFromLocalStorage)
+    }
+  },[])
+
+  const handleSubmit = async (e) => { // Submit Handler
     e.preventDefault()
 
     const { data } =await getSummary({ articleUrl: article.url})
 
     if(data?.summary){
       const newArticle = {...article, summary: data.summary}
+      const updatedAllArticle = [newArticle, ...allArticles]
+
       setArticle(newArticle)
-      console.log(newArticle)
+      setAllArticles(updatedAllArticle)
+
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticle))
     }
 
+  }
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl)
+    navigator.clipboard.writeText(copyUrl)
+    setTimeout(() => setCopied(false), 3000)
   }
 
   return (
@@ -48,9 +71,51 @@ function Demo() {
           </button>
         </form>
         {/* Browser URL History */}
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+          {allArticles.map((item, index) => (
+            <div 
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className='link_card'
+            >
+
+              <div className='copy_btn' onClick={()=> handleCopy(item.url)}>
+                <img 
+                  src={copied === item.url ? tick : copy}
+                  alt='copy_icon'
+                  className='w-[40%] h-[40%] object-contain'
+                />
+              </div>
+              <p className='flex-1 font-satoshi text-blue-700 font-medium text-small truncate'>{item.url}</p>
+
+            </div>
+          ))}
+        </div>
 
       </div>
       {/* Display Result */}
+      <div className='my-10 max-w-full justify-center items-center'>
+            {isFetching ? (
+              <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+            ) : error ? (
+              <p className='font-inter font-bold text-black text-center'>
+                Well, that wasn't supposed to happen...
+                <br />
+                <span className='font-satoshi font-normal text-gray-700'>{error?.data?.error}</span>
+              </p>
+            ) : (
+              article.summary && (
+                <div className='flex flex-col gap-3'>
+                  <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                    Article <span className='blue_gradient'>Summary</span>
+                  </h2>
+                  <div className='summary_box'>
+                    <p className='font-inter font-medium text-sm text-gray-700'>{article.summary}</p>
+                  </div>
+                </div>
+              )
+            )}
+      </div>
     </section>
   )
 }
